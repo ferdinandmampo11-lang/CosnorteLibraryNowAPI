f3372582849346dec64e945dfdbe86223351ab90
ferdinandmampo11-lang <ferdinandmampo11@gmail.com>
2026-05-09 18:54:44 +0800

    Update Program.cs

--- MuitLibraryNowAPI/Program.cs
+++ MuitLibraryNowAPI/Program.cs
@@ -10,15 +10,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-
-//app.Urls.Add($"http://+:{port}");
+app.Urls.Add($"http://+:{port}");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
